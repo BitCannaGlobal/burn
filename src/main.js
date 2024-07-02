1b22195a08f1e095e13dc755ff9da8e5665bb8de
atmoner <contact.atmoner@gmail.com>
2024-07-03 00:48:57 +0200

    Update burn UI

--- src/main.js
+++ src/main.js
@@ -2,5 +2,25 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import Countdown from 'vue3-flip-countdown'
 
-createApp(App).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  theme: {
+    defaultTheme: 'dark'
+  },
+  components,
+  directives,
+})
+
+const app = createApp(App);
+
+app.use(Countdown)
+app.use(vuetify)
+
+app.mount('#app')
